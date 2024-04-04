9e1877bc129dd505315deafdae7d0366e6c9c20a
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-04-04 16:25:23 +0100

    add tests for / endpoints

--- src/spec/index.spec.ts
+++ src/spec/index.spec.ts
@@ -4,16 +4,23 @@ import * as testData from "../db/data/test-data/index";
 import app from "../app";
 import seed from "../db/seeds/seed";
 import db from "../db/index";
+import endpoints = require("../endpoints.json")
 
 beforeEach(() => seed(testData));
 after(() => db.end());
 
-describe("GET /api/reviews", () => {
-    it("200: responds with an array of review objects", () => {
-        // return request(app)
-        //     .get("/api/reviews")
-        //     .expect(200)
-        //     .then(({ body }) => {
-        //     })
+describe("GET /", () => {
+    it("200: responds with an array endpoints", () => {
+        return request(app)
+        .get("/")
+        .expect(200)
+        .then((res) => {
+            console.log(endpoints)
+            const expectedLength = endpoints.length;
+            expect(res.body.endpoints).to.have.lengthOf(expectedLength)
+            res.body.endpoints.forEach(endpoint => {
+                expect(endpoint).to.include.all.keys("route", "description");
+            });
         })
+    })
     })
