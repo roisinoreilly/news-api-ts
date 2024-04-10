5021a55b5d17615182bb75f01966e54f3fc7495f
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-04-10 09:51:12 +0100

    add xata install files

--- dist/app.js
+++ dist/app.js
@@ -6,5 +6,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const api_router_1 = __importDefault(require("./routers/api.router"));
 const app = (0, express_1.default)();
-app.use("/", api_router_1.default);
+app.use("/api", api_router_1.default);
 exports.default = app;
