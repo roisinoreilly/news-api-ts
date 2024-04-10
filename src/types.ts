794bbd8d3cb91c613e791bc7cff90d8f3c0689cc
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-04-10 14:21:47 +0100

    add comments and user types

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,34 @@
 export type Topic = {
-    description: string;
-    slug: string;
-}
+  description: string;
+  slug: string;
+};
 
 export type Article = {
-    article_id: number;
-    title: string;
-    topic: string;
-    author: string;
-    body: string;
-    created_at: Date;
-    votes: number;
-    comment_count: number;
-}
+  article_id: number;
+  title: string;
+  topic: string;
+  author: string;
+  body: string;
+  created_at: Date;
+  votes: number;
+  comment_count: number;
+};
 
 export interface ResponseError extends Error {
-    status?: number;
-    code?: string;
-    msg?: string;
-  }
+  status?: number;
+  code?: string;
+  msg?: string;
+}
+
+export type User = {
+  username: string;
+  avatar_url: string;
+};
+
+export type Comment = {
+  comment_id: number;
+  author: User;
+  votes: number;
+  created_at: Date;
+  body: string;
+};
