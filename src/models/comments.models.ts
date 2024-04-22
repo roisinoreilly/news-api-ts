87c2ea3d727142fc722aabc010bb2f3d74019969
Roisin OReilly <roisin.oreilly1996@gmail.com>
2024-04-22 10:47:05 +0100

    remove comment

--- src/models/comments.models.ts
+++ src/models/comments.models.ts
@@ -14,16 +14,4 @@ export const deleteCommentById = (
                 ]);
             }
         });
-
-    // .query(`SELECT * FROM comments WHERE comment_id = $1`, [comment_id]).then(({ rows }: { rows: [] }) => {
-    //     if (rows.length === 0) {
-    //       return Promise.reject({ status: 404, msg: "Comment not found" });
-    //     }
-    //     else return db.query(
-    //         `DELETE FROM articles WHERE article_id = $1;`,
-    //         [comment_id]
-    //       ).then(() => {
-    //         return;
-    //       })
-    //   })
   }
